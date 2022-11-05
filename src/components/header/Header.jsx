a217d9fea5972f0e4e21b2eb04963e56a501a759
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-05 23:17:00 +0530

    chore: about section done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/baal.png'
+import ME from '../../assets/Pro.jpeg'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
