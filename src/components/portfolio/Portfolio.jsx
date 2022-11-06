a6addf226581a420e0165de0f1c1999e18b2e19c
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-06 21:17:37 +0530

    chore: portfolio section done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
 import './portfolio.css'
+import IMG from '../../assets/dalek.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG,
+    title: 'Dalek',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 2,
+    image: IMG,
+    title: 'Dalek',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 3,
+    image: IMG,
+    title: 'Dalek',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 4,
+    image: IMG,
+    title: 'Dalek',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 5,
+    image: IMG,
+    title: 'Dalek',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 6,
+    image: IMG,
+    title: 'Dalek',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id = 'portfolio'>Portfolio</section>
+    <section id = 'portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio_item">
+                <div className='portfolio_item-image'>
+                  <img src = {image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio_item-cta">
+                  <a href={github} className='btn' target="_blank">Github</a>
+                  <a href={demo} className='btn btn-primary' target="_blank">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
